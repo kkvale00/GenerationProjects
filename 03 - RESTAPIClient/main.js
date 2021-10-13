3eb83b4ee7f2525505d9e42691f8cf4d52c7b260
kkvale <marcocaccavale.napoletano@gmail.com>
2021-10-13 09:58:19 +0200

    creazione index e js

---
+++ 03 - RESTAPIClient/main.js
@@ -0,0 +1,13 @@
+// $ selezioniamo il DOM e per mezzo della funzione ready 
+// passiamo la nostra funziona al cui interno sara contenuta
+// tutta la logica dell'applicazione
+$(document).ready(init);
+    
+// in JS posso passare le funzioni come paramettri ad altre funzuione callback
+
+function init(){
+    console.log('Hello World');
+};
+
+
+
